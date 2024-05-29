70ec65d455479e01abc7b0e26582d4d1ed1a4036
tingweiou <kklb1716@gmail.com>
2024-05-29 21:28:26 +0800

    add readme file

--- src/ORB_SLAM2_NOETIC/src/Frame.cc
+++ src/ORB_SLAM2_NOETIC/src/Frame.cc
@@ -179,7 +179,7 @@ Frame::Frame(const cv::Mat &imGray, const cv::Mat &imDepth, const double &timeSt
     // sending data to server
     SendTwoSetsOfKeyPoints(clientSocket, mvKeysUn, mvKeys, mDescriptors);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     // receive keypoint data from server
     std::vector<cv::KeyPoint> received_kpUn;
